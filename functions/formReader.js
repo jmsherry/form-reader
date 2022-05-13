41b6471d5907e32923f050a2afe5c5c016879c5a
James Sherry <james.m.sherry@googlemail.com>
2022-05-13 04:21:59 +0100

    last fiddle

--- functions/formReader.js
+++ functions/formReader.js
@@ -32,12 +32,21 @@ exports.handler = async (event, context) => {
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <style>
+        .navbar {
+          margin-block-end: 3em;
+        }
+
+        table {
+          table-layout: fixed;
+        }
+      </style>
   </head>
   
   <body>
     <header>
       <div class="navbar navbar-light bg-light">
-        <div class="container-fluid">
+        <div class="container">
           <h1 class="navbar-brand mb-0">Form Data</h1>
         </div>
       </div>
