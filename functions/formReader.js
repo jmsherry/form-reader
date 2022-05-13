8a38e2e202d87c73f110d96a60217104c83d290d
James Sherry <james.m.sherry@googlemail.com>
2022-05-13 04:03:43 +0100

    styling and back button

--- functions/formReader.js
+++ functions/formReader.js
@@ -28,10 +28,11 @@ exports.handler = async (event, context) => {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
   <h1>Form Data</h1>
-  <table>
+  <table class="table">
     <thead>
       <tr>
         <th scope="col">Name</th>
@@ -42,6 +43,12 @@ exports.handler = async (event, context) => {
     ${tableRowsHTML}
     </tbody>
   </table>
+  <button id="back" class="btn btn-primary>Go back to your form</button>
+  <script>
+    document.getElementById('back').addEventListener('click', () => {
+      history.back();
+    });
+  </script>
 </body>
 </html>
   `;
