840e66435771043052d1f8e5ccabb131ab9e5ac9
James Sherry <james.m.sherry@googlemail.com>
2022-05-13 04:15:58 +0100

    more bootstrap stuff

--- functions/formReader.js
+++ functions/formReader.js
@@ -22,43 +22,50 @@ exports.handler = async (event, context) => {
 
   const HTML = `
   <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-  <header>
-    <div class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <h1 class="navbar-brand mb-0">Form Data</h1>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link data-n-head="true" rel="icon" type="image/x-icon" href="https://www.thejump.tech/favicon.ico">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+  
+  <body>
+    <header>
+      <div class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <h1 class="navbar-brand mb-0">Form Data</h1>
+        </div>
       </div>
-    </div>
-  </header>
-  <main>
-    <table class="table table-striped table-hover table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Value</th>
-        </tr>
-      </thead>
-      <tbody>
-      ${tableRowsHTML}
-      </tbody>
-    </table>
-    <button id="back" class="btn btn-primary">Go back to your form</button>
+    </header>
+    <main>
+      <div class="container">
+        <table class="table table-striped table-hover table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${tableRowsHTML}
+          </tbody>
+        </table>
+        <button id="back" class="btn btn-primary">Go back to your form</button>
+      </div>
+    </main>
     <script>
       document.getElementById('back').addEventListener('click', () => {
         history.back();
       });
     </script>
-  </main>
-</body>
-</html>
+  </body>
+  
+  </html>
   `;
 
   console.log({ HTML });
